b0d90bb22e66e73c0a02e3ddc6fb9b2da313b256
alpernai <148302974+alpernai@users.noreply.github.com>
2024-10-01 15:54:07 -0600

    Create 'createGrid' function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let reset = document.querySelector(".reset");
 let board = document.querySelector(".board");
-let cell = document.createElement("div");
+let gridSize;
 
 function getSize(){
     let size;
@@ -20,18 +20,32 @@ function getSize(){
 
     } while (isNaN(size) || size < 1 || size > 100 || !Number.isInteger(size));
 
-    console.log(size);
-    return size;
+    gridSize = size;
 }
 
-reset.addEventListener("click", getSize);
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
 
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell); 
+        }
+
+        board.appendChild(row);
+    }
+}
 
 // 'reset' stores the HTML reset button.
 // 'board' stores the HTML div that will contain the grid.
+// 'row' creates and stores a new div element. 
 // 'cell' creates and stores a new div element. 
-// 'getSize' prompts for and returns 'size'.
-// 'createGrid' 1. deletes previous grid 2. appends 'size' x 'size' ammount of 'cell' elements into 'board', 3. adds class 'gridItem' to elements. 
+// 'gridSize' will store the input of getSize. 
+// 'getSize' prompts user for input and returns 'size'.
+// 'createGrid' deletes previous grid and appends 'size' ammount of 'row' elements into 'board' and 'size' ammount of 'cell' elements into every 'row'. 
 // 'styleGrid' function styles every 'cell' element. 
+// 'colorGrid' changes the color of every grid element.
 // Click event listener for 'reset'. On click, execute functions 'getSize', 'createGrid' and 'styleGrid'. 
-// 'colorGrid' changes the color of every grid element when mouse hovers over it.  
+// Hover event listener for grid elements that executes 'colorGrid'.
